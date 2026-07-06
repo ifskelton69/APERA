b79ec03d5674cd7095c4600bf1cccf36431f16f1
ifskelton69 <adityabirajdar0305@gmail.com>
2026-07-07 04:09:02 +0530

    auth.controller.js changes

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -98,8 +98,8 @@ export async function login(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      sameSite: "strict",
-      secure: process.env.NODE_ENV === "production",
+      sameSite: "same",
+      secure: true,
     });
 
     res.status(200).json({ success: true, user: user });
