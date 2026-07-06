b62472710071c470afa958916ba3438ea735e138
ifskelton69 <adityabirajdar0305@gmail.com>
2026-07-07 03:16:11 +0530

    probably

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === 'development' ? 'https://apera.onrender.com/api' : '/api',
-  withCredentials: true,// to send cookies with requests
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "https://apera.onrender.com/api",
+  withCredentials: true,
 });
