5ad85ac7f305e5a3b27b20513c252a7b70cec19c
ifskelton69 <adityabirajdar0305@gmail.com>
2026-07-07 03:50:19 +0530

    cookie issue resolve

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -56,7 +56,7 @@ export async function signUp(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      sameSite: "strict",
+      sameSite: "none",
       secure: process.env.NODE_ENV === "production",
     });
 
