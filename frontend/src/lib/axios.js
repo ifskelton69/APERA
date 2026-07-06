cadf4f4fcbdab0891689793472c4d12070891cc2
ifskelton69 <adityabirajdar0305@gmail.com>
2026-07-07 03:10:17 +0530

    propababiliy final commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === 'development' ? 'https://apera.onrender.com' : '/api',
+  baseURL: import.meta.env.MODE === 'development' ? 'https://apera.onrender.com/api' : '/api',
   withCredentials: true,// to send cookies with requests
 });
